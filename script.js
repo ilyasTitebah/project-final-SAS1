c1dc84e84e763462625e51b553874e0442093bde
ILYAS TITEBAH <titebahilyess@gmail.com>
2026-09-08 18:24:08 +0200

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+switch (choix) {
+    case 1:afficher les trajets
+    case 2:acheter un ticket
+    case 3:afficher les ticket
+    case 4:annuler un ticket
+    case 5:rechercher un ticket
+    case 6:filtrer les trajets
+    case 7:trier les trajets
+}
